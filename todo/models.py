56fa5e1930cac4236f3d20abe62803c003242579
chrisq557 <chrisq557santos@gmail.com>
2025-07-20 17:57:47 +0100

    Create category model, make migration and migrate. Register to admin.py

--- todo/models.py
+++ todo/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Task(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='tasks')
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     scheduled_time = models.DateTimeField(null=True, blank=True)

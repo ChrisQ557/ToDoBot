47bb08ada3565b2b6f0d7df4ea158797e4166ea7
chrisq557 <chrisq557santos@gmail.com>
2025-07-23 11:30:00 +0100

    added task_detail view

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.TaskList.as_view(), name='home'),
+    path('<slug:slug>/', views.task_detail, name='task_detail'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Task
 
@@ -7,6 +7,20 @@ class TaskList(generic.ListView):
     queryset = Task.objects.order_by('user')
     template_name = 'todo/index.html'
     paginate_by = 9
+
+def task_detail(request, slug):
+    """
+    Display an individual :model:`todo.Task`.
+
+    Context
+        task
+            An instance of :model:`todo.Task`.
+
+    Template
+        todo/task_detail.html
+    """
+    task = get_object_or_404(Task, slug=slug)
+    return render(request, 'todo/task_detail.html', {'task': task})
 
     
    

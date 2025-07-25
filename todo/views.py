a38fa4948fd82b0e72140f5f9270489db10456c5
chrisq557 <chrisq557santos@gmail.com>
2025-07-25 21:43:46 +0100

    Add create and delete task views, templates, and UI buttons for full CRUD support

--- todo/views.py
+++ todo/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Task
 from .forms import TaskForm
 
 # Create your views here
-class TaskList(generic.ListView):
-    queryset = Task.objects.order_by('user')
+class TaskList(LoginRequiredMixin, generic.ListView):
     template_name = 'todo/index.html'
-    paginate_by = 9
+    paginate_by = 6
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user).order_by('-created_at')
 
 @login_required
 def task_detail(request, slug):

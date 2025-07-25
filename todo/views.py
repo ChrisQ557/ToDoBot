dd03c7d29f9cc1b29587997b3c572df7b3a4c205
chrisq557 <chrisq557santos@gmail.com>
2025-07-25 20:40:50 +0100

    Add crispy-bootstrap5 dependency and protect task_detail view with login_required

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.views import generic
 from .models import Task
+from .forms import TaskForm
 
 # Create your views here
 class TaskList(generic.ListView):
@@ -8,19 +10,30 @@ class TaskList(generic.ListView):
     template_name = 'todo/index.html'
     paginate_by = 9
 
+@login_required
 def task_detail(request, slug):
     """
-    Display an individual :model:`todo.Task`.
+    Display an individual :model:`todo.Task` and its form.
 
     Context
         task
             An instance of :model:`todo.Task`.
+        form
+            A form instance for :model:`todo.Task`.
 
     Template
         todo/task_detail.html
     """
-    task = get_object_or_404(Task, slug=slug)
-    return render(request, 'todo/task_detail.html', {'task': task})
+    task = get_object_or_404(Task, slug=slug, user=request.user)
+    form = TaskForm(instance=task)
+    return render(
+        request,
+        "todo/task_detail.html",
+        {
+            "task": task,
+            "form": form,
+        },
+    )
 
     
    
